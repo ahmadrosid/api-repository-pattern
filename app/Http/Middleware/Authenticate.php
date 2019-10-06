b9f0be074d84aaedfa19c796b7fabd018325c0ed
ahmadrosid <alahmadrosid@gmail.com>
2019-10-06 11:14:01 +0700

    fix: 🐛 use default authentication

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Contracts\Auth\Factory as Auth;
+use Illuminate\Http\Response;
 
 class Authenticate
 {
@@ -33,17 +34,11 @@ class Authenticate
      * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $guard = null)
     {
-        $token = last(explode(" ", $request->header('authorization')));
-
-        if ($this->auth->guard('api')->validate([
-            'access_token' => $token
-        ])) {
-            return response()->json('Unauthorized.', 401);
+        if ($this->auth->guard($guard)->guest()) {
+            return response('Unauthorized.', 401);
         }
-
-        $this->auth->setRequest($request);
 
         return $next($request);
     }
